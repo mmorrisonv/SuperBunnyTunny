9ae9462783d29cf096b8e781985ee2dbc1476fd2
Pride of Concept <info@prideofconcept.com>
2011-11-29 05:03:50 -0500

    adding more demo images | fleshed out listing and feature pages :: 12 hours 10pm-3pm  10pm - 5am

--- js/bbunny.js
+++ js/bbunny.js
@@ -5,8 +5,19 @@ $(window).load(function(){
 
 function adjustNavUniform(){
 	//console.log('test');
+
+	/*
 	var width = $('header .widthFull').width();
 	var count = $('header .widthFull li').length;
 
-	$('header .widthFull li').css('width',width / count);
+	$('header .widthFull li').css('width',width / count);*/
+
+	$.each($('.jsuniform'),function(index,value){
+		//console.log(this);
+		$this = $(this);
+		var width = $this.width();
+		var count = $this.children().length;
+		$this.children().css('width',width / count);
+	});
+
 }
