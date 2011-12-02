fdb1a46ca1d66a437b8ce09c56363187cfcc2415
mmorrisonv <mmorrison@virid.com>
2011-12-01 20:00:45 -0500

    1pm - 8pm - many fixes adn tweaks. detailed runn down of about 80% of group1.

--- assets/js/lib/modal.js
+++ assets/js/lib/modal.js
@@ -2,7 +2,10 @@ $(document).ready(function(){
 	
 	//activate modals
 	$('#my-modal,#modal-thankyou,#modal-email,#modal-wlistconfirm').modal({backdrop:true,keyboard:true});
-
+	//setup modal for zoom on product detail page
+	$('.zoombtn').click(function(){
+		window.open ("/pages/group1/zoom.html","mywindow","menubar=1,resizable=1,width=650,height=850");
+	});
 })
 $(window).load(function(){
 	
